7de4ed45f3d93c77d69a2e7790de900b43175231
Amit Pamecha <amitxpamecha@gmail.com>
2016-11-19 18:25:19 +0530

    adding Prototype Design Pattern Code

--- src/com/amitpamecha/prototype/demo/TestPrototypePattern.java
+++ src/com/amitpamecha/prototype/demo/TestPrototypePattern.java
@@ -7,16 +7,16 @@ public class TestPrototypePattern {
         User user = new User("User A", "USER Level", userAccessControl);
         System.out.println("************************************");
         System.out.println(user);
-        
+
         userAccessControl = AccessControlProvider.getAccessControlObject("USER");
         user = new User("User B", "USER Level", userAccessControl);
-        System.out.println("Changing access control of: "+user.getUserName());
+        System.out.println("Changing access control of: " + user.getUserName());
         user.getAccessControl().setAccess("READ REPORTS");
         System.out.println(user);
         System.out.println("************************************");
-        
+
         AccessControl managerAccessControl = AccessControlProvider.getAccessControlObject("MANAGER");
         user = new User("User C", "MANAGER Level", managerAccessControl);
         System.out.println(user);
-        }
+    }
 }
